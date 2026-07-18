595371d1598b8cf594605bed6e1064d8f7c408ee
Vivek Chauhan <vivekchauhan20150232922@gmail.com>
2026-07-19 01:13:28 +0530

    Add user registration API with password encryption

--- src/main/java/com/vivek/library/config/SecurityConfig.java
+++ src/main/java/com/vivek/library/config/SecurityConfig.java
@@ -2,8 +2,11 @@ package com.vivek.library.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -11,6 +14,20 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
 
+    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        System.out.println("SecurityFilterChain Loaded");
+        http.csrf(csrf -> csrf.disable());
+        http.authorizeHttpRequests(auth -> {auth.requestMatchers("/auth/**", "/swagger-ui/**", "/v3/api-docs/**")
+                .permitAll()
+                .anyRequest().authenticated();
+        });
+        http.sessionManagement(session ->
+                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        );
+        http.httpBasic(httpBasic -> httpBasic.disable());
+        return http.build();
     }
 
 }
